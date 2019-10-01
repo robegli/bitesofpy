7600e407b20260e43ba77785f3472fb4af3e1de5
robegli <rob.egli@gmail.com>
2019-09-30 22:34:11 -0500

    sub 005

--- 005/names.py
+++ 005/names.py
@@ -7,18 +7,18 @@ NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
 def dedup_and_title_case_names(names):
     """Should return a list of title cased names,
        each name appears only once"""
-    # new_names = []
-    # for name in names:
-    #     name = name.title()
-    #     if name not in new_names:
-    #         new_names.append(name)
-    # return new_names
-    return [name.title() for name in names if name.title() not in names]
+    new_names = []
+    for name in names:
+        name = name.title()
+        if name not in new_names:
+            new_names.append(name)
+    return new_names
 
 def sort_by_surname_desc(names):
     """Returns names list sorted desc by surname"""
     names = dedup_and_title_case_names(names)
-    # ...
+    names.sort(key=lambda s: s.split()[1], reverse=True)
+    return names
 
 
 def shortest_first_name(names):
@@ -26,8 +26,11 @@ def shortest_first_name(names):
        You can assume there is only one shortest name.
     """
     names = dedup_and_title_case_names(names)
-    # ...
+    firsts = [x.split()[0] for x in names]
+    return min(firsts)
 
 
 if __name__ == '__main__':
-    print(dedup_and_title_case_names(NAMES))
+    # print(dedup_and_title_case_names(NAMES))
+    # print(sort_by_surname_desc(NAMES))
+    print(shortest_first_name(NAMES))
